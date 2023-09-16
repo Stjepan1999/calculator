ed425d042db9c376264dd1444f1f28912bc75eee
Stjepan1999 <stjepanfranic99@gmail.com>
2023-09-16 15:28:17 +0200

    Create UI

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result
+}
+
+function subtract(firstNumber, secondNumber) {
+    let result = firstNumber - secondNumber;
+    return result
+}
+
+function multiply(firstNumber, secondNumber) {
+    let result = firstNumber * secondNumber;
+    return result
+}
+
+function divide(firstNumber, secondNumber) {
+    let result = firstNumber / secondNumber;
+    return result
+}
+
+function operate() {
+    
+}
